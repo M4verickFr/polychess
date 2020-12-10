52a368251f19de1d6a3a0985a91733d226d384bf
esteban143 <40337451+esteban143@users.noreply.github.com>
2020-12-10 12:06:17 +0100

    Update plochass.py
    Creation de fonction :
     - chessGame() pour le lancement de partie
     - initBoard() pour l'initialisation du plateau initial
     - displayBoard() pour l'affichage du plateau
     - chooseGameType() pour le choix du type de partie
     - startRound() pour commencer un tour
     - getPlayerMove() pour récupérer le mouvement d'un joueur
     - makeMove() pour deplacer un pion sur le plateau
    
    ---- Fin Scéance 1 -----

--- polychess.py
+++ polychess.py
@@ -1,29 +1,91 @@
 #python-chess import
 #https://github.com/niklasf/python-chess
 import chess
+#import polyglot as pg
 
-#set the board to its initial position
-#corresponding to: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
-board = chess.Board()
-
-#print the board on the console
-print(board)
 
 #SVG render for the board is possible in Jupyter Notebook
 #board
 
 #get all the legal moves for the current position
-moves = board.legal_moves
 
-#how many moves are available?
-print(moves.count())
+def initBoard():
+    return chess.Board()
 
+def displayBoard(board):
+    print(board)
+
+def chooseGameType():
+    #Display different type de partie
+    #JvsJ
+    #JvsIA
+    #...
+    gameType = 1
+    return gameType
+
+def startRound(board, gameType):
+    #Appeler le type de partie en fonction du type de partie
+    move = None
+
+    if(gameType == 1):
+        move = getPlayerMove(board)
+
+    current_board = makeMove(board, move)
+    return current_board
+
+def getPlayerMove(board):
+
+    validDeplacement = False
+    moves = board.legal_moves
+
+    print("----------------------------------------")
+    deplacement = input("De quelle case voulez vous partir et quelle tache voulez vous aller ?\n")
+
+    for move in list(moves):
+        if(deplacement == chess.Move.uci(move)):
+            validDeplacement = True
+    
+    if(not validDeplacement):
+        print("Erreur de saisie : aucun mouvement possible ne correspond a votre saisie")      
+        return getPlayerMove(board)
+
+    
+    
+
+    return deplacement
+
+    
+
+def makeMove(board,move):
+    deplacement = chess.Move.from_uci(move)
+    board.push(deplacement)
+
+    displayBoard(board)
+
+    return board
+
+def chessGame():
+    board = initBoard()
+    gameType = chooseGameType()
+    
+    displayBoard(board)
+
+    while not board.is_game_over():
+        board = startRound(board, gameType)
+
+
+
+    print("The game is over")
+    print(board.result())
+
+chessGame()
+"""
 #iterate over all the moves
 for move in moves: 
     
     #display the move
     print(move)
-    
+
     #save the current position
     current_board = board
     
@@ -39,8 +101,10 @@ for move in moves:
     #undo the move
     board.pop()
     
+
+
     #do we have a winner?
     if (board.is_game_over()):
         print("The game is over")
         print(board.result())
-    
+"""  
