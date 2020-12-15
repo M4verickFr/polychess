2d17220244d7b17223769c7d0119e9017c456459
esteban143 <40337451+esteban143@users.noreply.github.com>
2020-12-15 17:30:50 +0100

    Update polychess.py
    Upgrade minmax fonction
    Create random AI choice

--- polychess.py
+++ polychess.py
@@ -3,8 +3,7 @@
 import chess
 #import polyglot as pg
 import chess.polyglot 
-
-import minMax 
+import random
 
 #SVG render for the board is possible in Jupyter Notebook
 #board
@@ -18,27 +17,45 @@ def displayBoard(board):
     print(board)
 
 def chooseGameType():
-    #Display different type de partie
-    #JvsJ
-    #JvsIA
-    #...
-    gameType = 2
-    return gameType
+    #Display menu
+    #Select gameType
+    liste = [1,2]
+    erreur = False
+    print("------------------------------")
+    print("Choisir le mode de jeu :")
+    print("1- Joueur contre Joueur")
+    print("2- Joueur contre IA")
+    gameType = input("Saisissez le mode : ")
+
+    try:
+        if(not int(gameType) in liste):
+            erreur = True
+    except: 
+        erreur = True
+    if(erreur):
+        print("------------------------------")
+        print("Erreur de saisie")
+        chooseGameType()
+
+
+    return int(gameType)
+
 
 def startRound(board, playerType):
-    #Appeler le type de partie en fonction du type de partie
+    #Get the move related with the playerType
     move = None
-
+    print(board.turn)
     if(playerType == 1):
         move = getPlayerMove(board)
     elif(playerType == 2):
         move = getIAMove(board)
 
+    #Make a move
     current_board = makeMove(board, move)
     return current_board
 
 def getPlayerMove(board):
-
+    #Display player movement selection
     validDeplacement = False
     moves = board.legal_moves
 
@@ -52,29 +69,40 @@ def getPlayerMove(board):
     if(not validDeplacement):
         print("Erreur de saisie : aucun mouvement possible ne correspond a votre saisie")      
         return getPlayerMove(board)
+    
 
     return deplacement
 
 
-def getIAMove(board): #obtient le meilleur mouvement
+def getIAMove(board):
+    #Get AI Movement
     maxWeight = 0
-    deplacement = None 
+    deplacement = None
+
+    #Get movement in the polyglot
     with chess.polyglot.open_reader("bookfish.bin") as reader:
         for entry in reader.find_all(board):
             if maxWeight < entry.weight :
-
                 deplacement = entry.move
-                maxWeight = entry.weight   
-    if not deplacement:
-        deplacement = minMax.minMaxAlphaBeta(board,4,-3000,3000) #????????????????#
+                maxWeight = entry.weight
 
-    print(deplacement)
+    #If no deplacement
+    if not deplacement:
+        #Déplacement de l'IA de facon aléatoire
+        deplacement = random.choice(list(board.legal_moves))
+        #Déplacement de l'IA avec l'algorithme de minmax
+        #EN COURS
+        #val, deplacement = minmax(board,1)
+
+
+
     return(str(deplacement))
 
 
 
 def makeMove(board,move):
-    deplacement = chess.Move.from_uci(move)
+    #Make a move on the board
+    deplacement = chess.Move.from_uci(str(move))
     board.push(deplacement)
 
     displayBoard(board)
@@ -82,12 +110,17 @@ def makeMove(board,move):
     return board
 
 def chessGame():
+    #Create a game
+    #Initialization 
     board = initBoard()
+    #Choose the game type
     gameType = chooseGameType()
     
     displayBoard(board)
 
+    #If the game is not over
     while not board.is_game_over():
+        #Make a round related with the game type
         if gameType == 1:
             board = startRound(board, 1)
             if not board.is_game_over():
@@ -99,6 +132,69 @@ def chessGame():
 
     print("The game is over")
     print(board.result())
+
+
+def minmax(board, depth):
+    moves = list(board.legal_moves)
+    bestMove = None
+
+    if depth == 0 or len(moves) == 0:
+        return 0, None
+
+    print(depth)
+
+    if(not board.turn):
+        value = -9999
+        #print("ia")
+        for move in moves:
+
+            deplacement = chess.Move.from_uci(str(move))
+            #do the move
+            board.push(deplacement)
+            
+            val,current_move = minmax(board, depth-1)
+
+            #undo the move
+            board.pop()
+            
+            if(val > value):
+                value = val
+                bestMove = current_move
+
+    else:
+        #print("j")
+        value = 9999
+        for move in moves:
+    
+            #do the move
+            board.push(chess.Move.from_uci(str(move)))
+            
+            val,current_move = minmax(board, depth-1)
+
+            #undo the move
+            board.pop()
+            if(val < value):
+                value = val
+                bestMove = current_move
+
+    return value, bestMove
+
+
+    """
+    function minimax(node, depth, maximizingPlayer) is
+    if depth = 0 or node is a terminal node then
+        return the heuristic value of node
+    if maximizingPlayer then
+        value := −∞
+        for each child of node do
+            value := max(value, minimax(child, depth − 1, FALSE))
+        return value
+    else (* minimizing player *)
+        value := +∞
+        for each child of node do
+            value := min(value, minimax(child, depth − 1, TRUE))
+        return value
+    """
 
 chessGame()
 """
