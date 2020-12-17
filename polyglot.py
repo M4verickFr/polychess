45a6f5c9af5416d5094b01a720848a3f37b5dd1d
MaverickFR <49167110+M4verickFr@users.noreply.github.com>
2020-12-17 08:31:39 +0100

    create a function bestMove and test it

--- polyglot.py
+++ polyglot.py
@@ -1,16 +1,16 @@
-#python-chess import 
-#https://github.com/niklasf/python-chess
 import chess
-
-#used to access Polyglot book
 import chess.polyglot
 
-#set the initial position
-board = chess.Board()
+def bestMove(board, debug = False):
+    weight = 0
+    bestMove = None
+    with chess.polyglot.open_reader("bookfish.bin") as reader:
+        for entry in reader.find_all(board):
+            if entry.weight > weight:
+                weight=entry.weight
+                bestMove=entry.move
+    return bestMove
 
-#access the Polyglot book
-with chess.polyglot.open_reader("bookfish.bin") as reader:
-    for entry in reader.find_all(board):
-        print(entry.move, entry.weight, entry.learn)
- 
- 
+# Testing our code
+board = chess.Board()
+print(bestMove(board, debug=True))
